758c0321a8eabcb295a8465864d0a79e707c4c17
ruby 19 <ruby.19@metralha.cec.ime.usp.br>
2017-01-26 21:38:45 -0200

    commit crud

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,60 @@
+class MoviesController < ApplicationController
+  def index
+    (@movies =Movie.all)
+  end
+
+  def show
+    id= params['id']
+    @movie=Movie.find id
+  rescue ActiveRecord::RecordNotFound
+    @movie = nil
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+      @movie = Movie.new(movie_param)
+      if  @movie.save
+        redirect_to action: :show, id: @movie.id
+      else
+          render :new
+      end
+
+  end
+
+  def movie_param
+    params.require(:movie).permit(:title, :release_date, :description)
+  end
+
+  def edit
+
+    id = params["id"]
+    @movie = Movie.find (id)
+  rescue ActiveRecord::RecordNotFound
+    @movie = nil
+    rende file: "#{Rails.root}/public/404.htmll"
+  end
+
+  def update
+  begin
+    id = params["id"]
+    @movie = Movie.find (id)
+
+      @movie.update(movie_param)
+      if  @movie.save
+        redirect_to action: :show, id: @movie.id
+      else
+          render :new
+      end
+    rescue ActiveRecord::RecordNotFound
+      @movie = nil
+      rende file: "#{Rails.root}/public/404.htmll"
+    end
+
+  end
+
+
+end
